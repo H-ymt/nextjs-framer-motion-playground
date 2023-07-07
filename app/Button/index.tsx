d49c36db1a711406d6cd06227c6193a3a3a42a7f
H-ymt <107012772+H-ymt@users.noreply.github.com>
2023-07-08 00:11:58 +0900

    fix font-family

--- app/Button/index.tsx
+++ app/Button/index.tsx
@@ -1,6 +1,9 @@
 "use client";
 import Link from "next/link";
 import styles from "./index.module.scss";
+import { Noto_Sans_JP } from "next/font/google";
+
+const NotoSans = Noto_Sans_JP({ subsets: ["latin"], weight: "400" });
 
 type Props = {
   href: string;
@@ -9,7 +12,7 @@ type Props = {
 
 export default function Button({ href, children }: Props) {
   return (
-    <Link href={href}>
+    <Link href={href} className={NotoSans.className}>
       <button className={styles.button}>{children}</button>
     </Link>
   );
